6b78042303e07e262bdf26da6a6b8b36dd6a7567
William0831 <williamwangg0831@gmail.com>
2025-08-27 23:40:30 +1000

    Merged backend and front-end

--- backend/controllers/userProfilesController.js
+++ backend/controllers/userProfilesController.js
@@ -14,4 +14,37 @@ const getUserProfiles = async function(req,res){
      }
 }
 
-module.exports = {getUserProfiles};
+const getUserProfileById = async function(req,res,next){
+    const id = req.params.userId;
+    if(!id){
+        return res.status(400).send('User id is required')
+    }
+    try {
+        const response = await axios.get(`${USER_PROFILES_API_URL}/${id}`);
+        const userProfile = response.data;
+        res.status(200).json({
+            msg:'Get userProfile succeed',
+            data:userProfile
+        })
+    } catch (error) {
+         next(error)
+    }
+}
+const patchUserProfileById = async function(req,res,next){
+    const id = req.params.userId
+    if(!id){
+        return res.status(400).send('User id is required')
+    }
+    const newUerProfileData = req.body;
+    try {
+        const response = await axios.patch(`${USER_PROFILES_API_URL}/${id}`);
+        const updateUserProfile = response.data;
+        res.status(200).json({
+            msg:'Update user profile successfully',
+            data:updateUserProfile
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports = {getUserProfiles,getUserProfileById,patchUserProfileById};
--- backend/routes/userProfiles.js
+++ backend/routes/userProfiles.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getUserProfiles} = require('../controllers/userProfilesController');
+const {getUserProfiles,getUserProfileById,patchUserProfileById} = require('../controllers/userProfilesController');
 const userProfilesRouter = express.Router();
 
 // get user profiles.  /api/userProfiles
@@ -7,6 +7,8 @@ userProfilesRouter.get('/',getUserProfiles)
 
 
 // get user profiles by id    /api/userProfiles/:userId
+userProfilesRouter.get('/:userId',getUserProfileById)
 
-
+//PATCH update user profile by id   /api/userProfiles/:userId
+userProfilesRouter.patch('/:userId',patchUserProfileById)
 module.exports = userProfilesRouter;
